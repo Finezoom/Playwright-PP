7b4bcf49ef2a0d0a4f181d1a29cebde5639d2a32
prafull059 <127289358+prafull059@users.noreply.github.com>
2023-05-19 11:11:38 +0530

    Update QuilyzStaticTesting.spec.ts

--- Static Testing/QuilyzStaticTesting.spec.ts
+++ Static Testing/QuilyzStaticTesting.spec.ts
@@ -13,9 +13,10 @@ test('Static Comparison of Quiklyz Home Page ', async ({page}) =>{
 
     await page.goto("https://uat.quiklyz.com/")
     await page.locator('[class="Chennai col-md ng-star-inserted"]').click();
-
+    await page.pause()
+   
     expect(await page.screenshot({
-      fullPage:true,mask: [page.locator(' //*[@id="main-content-wrapper"]/fg-client-main-nav/mat-sidenav-container/mat-sidenav-content/div[2]/fg-form/div[2]/div/div/form/div/div[1]/fieldset/div/div[1]/div/fg-custom-container/div/div/div/div/fg-client-carousel/div/div/div/div[1]/div/ul/li[3]/div/div[1]/div/img')]
+      fullPage:true,mask:[page.locator(' //*[@id="main-content-wrapper"]/fg-client-main-nav/mat-sidenav-container/mat-sidenav-content/div[2]/fg-form/div[2]/div/div/form/div/div[1]/fieldset/div/div[1]/div/fg-custom-container/div/div/div/div/fg-client-carousel/div/div/div/div[1]/div/ul/li[3]/div/div[1]/div/img')]
     })).toMatchSnapshot("Home.png")
 
    
@@ -48,6 +49,7 @@ test('Static Comparison of Quiklyz Register Page ', async ({page}) =>{
   await page.getByText('Bengaluru', { exact: true }).click()
   await page.getByRole('button', { name: 'Login ' }).click()
   await page.getByRole('menuitem', { name: 'New user? Start here' }).click()
+  await page.waitForTimeout(5000)
   expect(await page.screenshot({
     fullPage:true
   })).toMatchSnapshot("RegisterPage.png")
@@ -57,16 +59,18 @@ test('Static Comparison of Quiklyz Login Page ', async ({page}) =>{
   await page.getByText('Bengaluru', { exact: true }).click()
   await page.getByRole('button', { name: 'Login ' }).click()
   await page.getByRole('button', { name: 'Login', exact: true }).click()
+  await page.waitForTimeout(5000)
 expect(await page.screenshot({
     fullPage:true
   })).toMatchSnapshot("LoginPage.png")
 })
 test('Static Comparison of Quiklyz Pan Verification Page', async ({page}) =>{  
   await page.goto('https://uat.quiklyz.com/', { waitUntil: 'networkidle' });
+  
   await page.getByText('Bengaluru', { exact: true }).click()
   await page.getByRole('button', { name: 'Login ' }).click()
   await page.getByRole('button', { name: 'Login', exact: true }).click()
-  await page.getByRole('tabpanel', { name: 'Login' }).getByText('Enter Mobile Number').fill("9126203443")
+  await page.getByRole('tabpanel', { name: 'Login' }).getByText('Enter Mobile Number').fill("9126203446")
   await page.getByRole('button', { name: 'Get OTP' }).click()
   for (let i = 1; i <= 6; i++) {
     await page.locator(`div:nth-child(${i}) > .form-control`).first().fill(`${i}`);
@@ -75,19 +79,20 @@ test('Static Comparison of Quiklyz Pan Verification Page', async ({page}) =>{
   await page.getByRole('link', { name: 'Find Cars' }).click()
   await page.getByRole('link', { name: 'Maruti Suzuki Swift' }).click()
   await page.waitForTimeout(2000)
-  await page.pause()
   //await page.click('//*[@id="main-content-wrapper"]/fg-client-main-nav/mat-sidenav-container/mat-sidenav-content/div[2]/fg-form/div[2]/div/div/form/div/div[1]/fieldset/div/div[2]/div/fg-custom-container/div/div/div/div/div[1]/div/div/div[2]/div/fg-custom-container/div/div/div[1]/div/div[1]/div[3]/div/div/div/fg-custom-container/div/div/div/div/fg-card/div/mat-card/mat-card-content/div/div[10]/div/div[1]/div[1]/div/div[1]/div/fg-custom-container/div/div/div/div/fg-single-action/fg-action-builder-actions/button')
   await page.locator('mat-card-content').filter({ hasText: 'Maruti Suzuki Swift Petrol Manual Metallic Select Variant Lxi Select Variant Pet' }).getByRole('button', { name: 'Edit Record' }).click()
+ await page.waitForTimeout(20000)
   expect(await page.screenshot({
     fullPage:true
   })).toMatchSnapshot("PanVerificationPage.png")
 })
-test.only('Static Comparison Testing of Quiklyz Address Check Page', async ({page}) =>{  
+test('Static Comparison Testing of Quiklyz Address Check Page', async ({page}) =>{  
   await page.goto('https://uat.quiklyz.com/', { waitUntil: 'networkidle' });
+  
   await page.getByText('Bengaluru', { exact: true }).click()
   await page.getByRole('button', { name: 'Login ' }).click()
   await page.getByRole('button', { name: 'Login', exact: true }).click()
-  await page.getByRole('tabpanel', { name: 'Login' }).getByText('Enter Mobile Number').fill("9126203443")
+  await page.getByRole('tabpanel', { name: 'Login' }).getByText('Enter Mobile Number').fill("9126203446")
   await page.getByRole('button', { name: 'Get OTP' }).click()
   for (let i = 1; i <= 6; i++) {
     await page.locator(`div:nth-child(${i}) > .form-control`).first().fill(`${i}`);
@@ -104,7 +109,7 @@ await page.getByRole('button', { name: 'June 1994' }).click()
 await page.getByRole('button', { name: '10 June 1994', exact: true }).click()
 await page.click('//*[@id="mat-checkbox-6"]/label/span[1]')
 await page.getByRole('button', { name: 'Edit Record' }).click()
-
+await page.waitForTimeout(20000)
   expect(await page.screenshot({
     fullPage:true
   })).toMatchSnapshot("AddressCheckPage.png")
